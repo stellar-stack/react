b62c40400f027654782919e4ff3db8b232ac1bf6
elliot <elliotgould5721@gmail.com>
2024-08-22 13:00:40 +0100

    installed uuid with npm and used arrays to loop throught different cards

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,33 +1,64 @@
 import "./index.css";
 import Employee from "./components/Employee";
-import {useState} from 'react';
+import { useState } from "react";
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
-  const [role, setRole] = useState('dev');
-  const [name, setName] = useState('');
+  const [role, setRole] = useState("dev");
+  const [name, setName] = useState("");
+
+  const [employees, setEmployess] = useState([
+    {
+      name: "Caleb",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/11883261/pexels-photo-11883261.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      name: "Sal",
+      role: "Manager",
+      img: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      name: "John",
+      role: "Software-engineer",
+      img: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      name: "Melania",
+      role: "Web-developer",
+      img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      name: "Corey",
+      role: "designer",
+      img: "https://images.pexels.com/photos/2128807/pexels-photo-2128807.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      name: "Jake",
+      role: "manager",
+      img: "https://images.pexels.com/photos/2613260/pexels-photo-2613260.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+  ]);
   // let role = 'dev';
   const showEmployee = true;
   return (
-    <div className="App bg-red-300" >
+    <div className="App ">
       {showEmployee ? (
         <>
-          <input
-            type="text"
-            onChange={(e) => {
-              console.log(e.target.value);
-              setRole(e.target.value);
-            }}
-          ></input>
-          <input
-            type="text" placeholder="name"
-            onChange={(e) => {
-              console.log(e.target.value);
-              setName(e.target.value);
-            }}
-          ></input>
-          <Employee name={name} role="Intern" />
-          <Employee name="Abbey" role={role} />
-          <Employee name="john" />
+          <div className="flex flex-wrap justify-center">
+            {employees.map((employee) => {
+              console.log(uuidv4());
+
+              return (
+                <Employee
+                key = {uuidv4()}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                />
+              );
+            })}
+          </div>
         </>
       ) : (
         <p>You cannot see the employees</p>
